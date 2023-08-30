941dd2cde3e82d1d2cc191970ab1745c7167c3c7
Jaroslav Adam <jaroslav.adam@cern.ch>
2023-08-30 13:16:36 +0200

    QCal1 Cherenkov calorimeter, files

---
+++ calorimeters/include/QCal1.h
@@ -0,0 +1,36 @@
+
+#ifndef QCal1_h
+#define QCal1_h
+
+#include "Detector.h"
+#include "G4VSensitiveDetector.hh"
+
+#include "CalPWOHits.h"
+
+class QCal1 : public Detector, public G4VSensitiveDetector {
+
+  public:
+
+    QCal1(const G4String&, GeoParser*, G4LogicalVolume*);
+
+    //called via Detector
+    virtual const G4String& GetName() const {return fNam;}
+    void CreateOutput(TTree*);
+    void ClearEvent();
+    void FinishEvent();
+
+    //called via G4VSensitiveDetector
+    virtual G4bool ProcessHits(G4Step *step, G4TouchableHistory*);
+
+  private:
+
+    G4LogicalVolume* MakeCell(GeoParser *geo);
+
+    G4String fNam; // name of detector sensitive logical volume
+
+    CalPWOHits::Coll fHits; // hits in fibers as in PWO
+
+};
+
+#endif
+
